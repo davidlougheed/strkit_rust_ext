5131dc1d1ffc06cc3d213104adf9745df7f31f5c
David Lougheed <david.lougheed@gmail.com>
2023-04-09 09:20:11 -0400

    rm slower form and rename SNV func to get_snvs_simple

--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,7 @@ use pyo3::types::PyDict;
 use std::collections::HashMap;
 
 #[pyfunction]
-fn mk_snvs_dict<'a>(
-    py: Python<'a>,
-    query_sequence: &str,
-    pairs: Vec<(usize, usize)>,
-    ref_seq: &str,
-    ref_coord_start: usize,
-    tr_start_pos: usize,
-    tr_end_pos: usize,
-) -> PyResult<&'a PyDict> {
-    let qry_seq_bytes = query_sequence.as_bytes();
-    let ref_seq_bytes = ref_seq.as_bytes();
-
-    let out = PyDict::new(py);
-
-    for (read_pos, ref_pos) in pairs {
-        if !(tr_start_pos <= ref_pos && ref_pos < tr_end_pos)
-            && (qry_seq_bytes[read_pos] != ref_seq_bytes[ref_pos - ref_coord_start])
-        {
-            out.set_item(ref_pos, qry_seq_bytes[read_pos] as char)?;
-        }
-    }
-
-    Ok(out)
-}
-
-#[pyfunction]
-fn mk_snvs_hash(
+fn get_snvs_simple(
     query_sequence: &str,
     pairs: Vec<(usize, usize)>,
     ref_seq: &str,
@@ -52,7 +26,6 @@ fn mk_snvs_hash(
 
 #[pymodule]
 fn strkit_rust_ext(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(mk_snvs_dict, m)?)?;
-    m.add_function(wrap_pyfunction!(mk_snvs_hash, m)?)?;
+    m.add_function(wrap_pyfunction!(get_snvs_simple, m)?)?;
     Ok(())
 }
--- strkit_rust_ext.pyi
+++ strkit_rust_ext.pyi
@@ -1,14 +1,4 @@
-def mk_snvs_dict(
-    query_sequence: str,
-    pairs: list[tuple[int, int]],
-    ref_seq: str,
-    ref_coord_start: int,
-    tr_start_pos: int,
-    tr_end_pos: int,
-) -> dict[int, str]: ...
-
-
-def mk_snvs_hash(
+def get_snvs_simple(
     query_sequence: str,
     pairs: list[tuple[int, int]],
     ref_seq: str,
