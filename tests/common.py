7b071d21aee591d4e2aa60e219be353e3838d863
David Lougheed <david.lougheed@gmail.com>
2024-02-10 11:15:30 -0500

    test: fix tests by switching to numpy arrays

--- tests/common.py
+++ tests/common.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 REF_SEQ = "ACACACATGGCCATAC"
 Q_SEQ = "ATCCCAAA"
 
@@ -5,8 +7,8 @@ Q_SEQ = "ATCCCAAA"
 PAIRS = [(0, 1000), (1, 1001), (2, 1003), (3, 1004), (4, 1005), (5, 1006), (6, 1008), (7, 1009)]
 SNVS = ((1003, "C"), (1009, "A"))
 
-ALIGN_COORDS_Q = list(map(lambda x: x[0], PAIRS))
-ALIGN_COORDS_R = list(map(lambda x: x[1], PAIRS))
+ALIGN_COORDS_Q = np.fromiter(map(lambda x: x[0], PAIRS), dtype=np.uint64)
+ALIGN_COORDS_R = np.fromiter(map(lambda x: x[1], PAIRS), dtype=np.uint64)
 
 SNV_CATALOG = [
     (1003, "snp1003", "G", ["C"]),
