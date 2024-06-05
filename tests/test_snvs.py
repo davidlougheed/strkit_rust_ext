1e223440ff1eb8c9c9098d8e281886e922721b2c
David Lougheed <david.lougheed@gmail.com>
2024-06-05 13:46:49 -0400

    tests: fix snv tests

--- tests/test_snvs.py
+++ tests/test_snvs.py
@@ -1,7 +1,13 @@
 from strkit_rust_ext import get_read_snvs
-from .common import REF_SEQ, Q_SEQ, ALIGN_COORDS_Q, ALIGN_COORDS_R, SNVS
+from .common import REF_SEQ, Q_SEQ, Q_QUALS, ALIGN_COORDS_Q, ALIGN_COORDS_R, SNVS
 
 
 def test_get_read_snvs():
-    r_items = tuple(sorted(get_read_snvs(Q_SEQ, REF_SEQ, ALIGN_COORDS_Q, ALIGN_COORDS_R, 994, 994, 1000, 0, 5, 20, 10, 0.0).items()))
+    r_items = tuple(
+        sorted(
+            get_read_snvs(
+                Q_SEQ, Q_QUALS, REF_SEQ, ALIGN_COORDS_Q, ALIGN_COORDS_R, 994, 994, 1000, 0, 5, 20, 10, 0.0
+            ).items()
+        )
+    )
     assert SNVS == r_items
