42f33965de8dd2f1f69c12b0ebea99f0aa4b7604
David Lougheed <david.lougheed@gmail.com>
2024-06-05 13:51:46 -0400

    tests: fix tests 2

--- tests/common.py
+++ tests/common.py
@@ -2,11 +2,11 @@ import numpy as np
 
 REF_SEQ = "ACACACATGGCCATAC"
 Q_SEQ = "ATCCCAAA"
-Q_QUALS = [90] * len(Q_SEQ)
+Q_QUALS = np.fromiter([90] * len(Q_SEQ), dtype=np.uint8)
 
 #         A  A       T  T       C  G       C  C       C  C       A  A       A  A       A  C
 PAIRS = [(0, 1000), (1, 1001), (2, 1003), (3, 1004), (4, 1005), (5, 1006), (6, 1008), (7, 1009)]
-SNVS = ((1003, "C"), (1009, "A"))
+SNVS = (1003, ("C", 90)), (1009, ("A", 90))
 
 ALIGN_COORDS_Q = np.fromiter(map(lambda x: x[0], PAIRS), dtype=np.uint64)
 ALIGN_COORDS_R = np.fromiter(map(lambda x: x[1], PAIRS), dtype=np.uint64)
