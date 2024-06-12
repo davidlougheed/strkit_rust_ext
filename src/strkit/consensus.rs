5550a83304be9de441d477046e20248f406889ae
David Lougheed <david.lougheed@gmail.com>
2024-06-12 12:20:29 -0400

    perf: reduce memory copying in POA consensus method

--- src/strkit/consensus.rs
+++ src/strkit/consensus.rs
@@ -28,55 +28,53 @@ pub fn best_representatives(seqs: Vec<&str>) -> HashSet<&str> {
 
 #[pyfunction]
 pub fn consensus_seq(seqs: Vec<&str>) -> Option<String> {
+    let n_seqs = seqs.len();
+
+    if n_seqs == 0 {
+        return None;
+    }
+
     panic::catch_unwind(|| {
 
         let scoring = Scoring::new(-1, 0, |a: u8, b: u8| if a == b { 1i32 } else { -1i32 });
 
-        let _seqs: Vec<&[u8]> = seqs.iter().map(|s| s.as_bytes()).collect();
+        let _seqs: Vec<&[u8]> = seqs.into_iter().map(|s| s.as_bytes()).collect();
 
-        if _seqs.len() == 1 {
-            return Some(_seqs[0].iter().map(|&b| b as char).collect::<String>());
+        let first_seq = _seqs[0];
+
+        if n_seqs == 1 {
+            return Some(first_seq.iter().map(|&b| b as char).collect::<String>());
         }
 
-        let mut _seqs_iter: Vec<&[u8]> = _seqs.clone();
+        let mut aligner = Aligner::new(scoring, first_seq);
 
-        let mut max_len: usize = 0;
-        let mut n_seqs: usize = 0;
+        let mut max_len: usize = first_seq.len();
+        _seqs[1..].iter().for_each(|y| {
+            max_len = cmp::max(max_len, y.len());
+            aligner.global(y).add_to_graph();
+        });
 
-        _seqs_iter.pop().map(|x| {
-            max_len = cmp::max(max_len, x.len());
-            n_seqs += 1;
+        let pretty = aligner
+            .alignment()
+            .pretty(aligner.consensus().as_slice(), _seqs, aligner.graph(), max_len * 4);
 
-            let mut aligner = Aligner::new(scoring, x);
+        let pretty_split = pretty.split('\n').skip(1);
 
-            for y in _seqs_iter {
-                max_len = cmp::max(max_len, y.len());
-                aligner.global(y).add_to_graph();
-                n_seqs += 1;
-            }
+        let mut aligned_seqs: Vec<&[u8]> = Vec::with_capacity(n_seqs);
+        let mut max_aligned_len: usize = 0;
 
-            let pretty = aligner
-                .alignment()
-                .pretty(aligner.consensus().as_slice(), _seqs, aligner.graph(), max_len * 4);
+        for s in pretty_split.into_iter().filter(|&z| !z.is_empty()) {
+            let s_seq = s.split('\t').nth(1).unwrap().as_bytes();
+            max_aligned_len = cmp::max(max_aligned_len, s_seq.len());
+            aligned_seqs.push(s_seq);
+        }
 
-            let pretty_split = pretty.split('\n').skip(1);
-
-            let mut aligned_seqs: Vec<&str> = Vec::with_capacity(n_seqs);
-            let mut max_aligned_len: usize = 0;
-
-            for s in pretty_split.filter(|&z| !z.is_empty()) {
-                let s_seq = s.split('\t').nth(1).unwrap();
-                max_aligned_len = cmp::max(max_aligned_len, s_seq.len());
-                aligned_seqs.push(s_seq);
-            }
-
-            let mut consensus_chars: Vec<char> = Vec::with_capacity(max_aligned_len);
-
-            for i in 0..max_aligned_len {
+        Some(
+            (0..max_aligned_len).into_iter().filter_map(|i| {
                 let mut counter = [0usize; 256];
-                for &s in aligned_seqs.iter() {
-                    counter[s.as_bytes()[i] as usize] += 1;
-                }
+                aligned_seqs.iter().for_each(|&s| {
+                    counter[s[i] as usize] += 1;
+                });
                 let mode_idx = counter
                     .iter()
                     .enumerate()
@@ -84,14 +82,8 @@ pub fn consensus_seq(seqs: Vec<&str>) -> Option<String> {
                     .map(|(idx, _)| idx)
                     .unwrap();
 
-                if mode_idx != 255 && mode_idx != GAP_CHAR_ORD {
-                    // Will be 255 if everything remained 0 somehow
-                    // We don't want gap characters in the consensus
-                    consensus_chars.push(mode_idx as u8 as char);
-                }
-            }
-
-            consensus_chars.iter().collect::<String>()
-        })
+                (mode_idx != 255 && mode_idx != GAP_CHAR_ORD).then_some(mode_idx as u8 as char)
+            }).collect::<String>()
+        )
     }).ok()?
 }
