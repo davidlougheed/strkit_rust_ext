b7ae178e2e9213623a855c095802cb5ee07a59bc
David Lougheed <david.lougheed@gmail.com>
2024-09-19 14:02:38 -0400

    test: fix python tests

--- tests/test_consensus.py
+++ tests/test_consensus.py
@@ -5,27 +5,18 @@ from strkit_rust_ext import consensus_seq
 logger = logging.getLogger(__name__)
 
 
-def test_best_representatives():
-    assert best_representatives(()) == set()
-    assert best_representative(()) == None
-
-    assert best_representatives(("AA", "AB", "AA")) == {"AA"}
-    assert best_representative(("AA", "AB", "AA")) == "AA"
-
-    assert best_representatives(("AAACAAA", "AACAAA", "AAACAA")) == {"AAACAAA"}
-    assert best_representative(("AAACAAA", "AACAAA", "AAACAA")) == "AAACAAA"
-
-
 test_params = [
     ((), None),
     (("", "", "", "", ""), ("", "single")),
     (("AA", "AB", "AA"), ("AA", "poa")),
-    (("", "AA", "AA"), ("AA", )),
-    (("", "A", "A", "A", "A"), ("A", "single")),
+    (("", "AA", "AA"), ("AA", "poa")),
+    (("A", "A", "A", "A"), ("A", "single")),
+    (("", "A", "A", "A", "A"), ("A", "best_rep")),
     (("", "A", "A", "A", "B"), ("A", "best_rep")),
+    (("AAACAAA", "AACAAA", "AAACAA"), ("AAACAAA", "poa")),
 ]
 
 
-@pytest.mark.parametrize(("seqs", "res"))
+@pytest.mark.parametrize(("seqs", "res"), test_params)
 def test_consensus(seqs, res):
     assert consensus_seq(seqs, logger, 100) == res
