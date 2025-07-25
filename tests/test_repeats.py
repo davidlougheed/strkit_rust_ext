d0d4e681dde43dd34fbbdc6aac06b47ff0a2462f
David Lougheed <david.lougheed@gmail.com>
2025-07-25 11:26:40 -0400

    test: more tests for get_repeat_count

--- tests/test_repeats.py
+++ tests/test_repeats.py
@@ -2,4 +2,6 @@ from strkit_rust_ext import get_repeat_count
 
 
 def test_get_repeat_count():
-    assert get_repeat_count(5, "CACACACACA", "ATGC", "ATGC", "CA", 50, 3, 1) == ((5, 36), 5, 0.0)
+    assert get_repeat_count(5, "CACACACACA", "ATGC", "ATGC", "CA", 50, 3, 1) == ((5, 36), 1, 0)
+    assert get_repeat_count(5, "CACACACA", "ATGC", "ATGC", "CA", 50, 3, 1) == ((4, 32), 9, -1)
+    assert get_repeat_count(4, "CACACCCC", "ATGC", "ATGC", "CA", 50, 3, 1) == ((4, 14), 9, 0)
