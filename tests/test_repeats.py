fb33f8663fd96ab461a8e324289b04f8c5aa74bb
David Lougheed <david.lougheed@gmail.com>
2025-07-29 13:42:08 -0400

    test: fix test

--- tests/test_repeats.py
+++ tests/test_repeats.py
@@ -3,5 +3,5 @@ from strkit_rust_ext import get_repeat_count
 
 def test_get_repeat_count():
     assert get_repeat_count(5, "CACACACACA", "ATGC", "ATGC", "CA", 50, 3, 1) == ((5, 36), 1, 0)
-    assert get_repeat_count(5, "CACACACA", "ATGC", "ATGC", "CA", 50, 3, 1) == ((4, 32), 9, -1)
-    assert get_repeat_count(4, "CACACCCC", "ATGC", "ATGC", "CA", 50, 3, 1) == ((4, 14), 9, 0)
+    assert get_repeat_count(5, "CACACACA", "ATGC", "ATGC", "CA", 50, 3, 1) == ((4, 32), 5, -1)
+    assert get_repeat_count(4, "CACACCCC", "ATGC", "ATGC", "CA", 50, 3, 1) == ((4, 14), 5, 0)
