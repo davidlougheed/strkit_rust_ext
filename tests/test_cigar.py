aec0bcb8e8da252ce74b499ad6fb92605dd267fc
David Lougheed <david.lougheed@gmail.com>
2025-07-29 13:48:12 -0400

    test: fix cigar test

--- tests/test_cigar.py
+++ tests/test_cigar.py
@@ -5,5 +5,6 @@ from .cigar import get_aligned_pairs_from_cigar
 
 def test_cigar():
     pairs1 = list(get_aligned_pairs_from_cigar(CIGAR_OPS, 0, 100000, True))
-    pairs2 = list(zip(*get_aligned_pair_matches(CIGAR_OPS, 0, 100000)))
+    ac = get_aligned_pair_matches(CIGAR_OPS, 0, 100000)
+    pairs2 = list(zip(ac.query_coords, ac.ref_coords))
     assert pairs1 == pairs2
