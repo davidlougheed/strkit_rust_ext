e93fdcce469a7cc339c4d6f585c20679f56de090
David Lougheed <david.lougheed@gmail.com>
2025-08-07 11:59:19 -0400

    feat(utils): port calculate_seq_with_wildcards to rust

--- src/strkit/utils.rs
+++ src/strkit/utils.rs
@@ -1,4 +1,6 @@
-use pyo3::prelude::*;
+use numpy::{ndarray::Array1, PyArray1, PyArrayMethods};
+use pyo3::{prelude::*, pybacked::PyBackedStr};
+use std::iter::zip;
 use crate::aligned_coords::STRkitAlignedCoords;
 
 pub fn find_coord_idx_by_ref_pos(
@@ -20,4 +22,32 @@ pub fn find_coord_idx_by_ref_pos_py(
     start_left: usize,
 ) -> (usize, bool) {
     find_coord_idx_by_ref_pos(&aligned_coords.borrow(), target, start_left)
+}
+
+// TODO: use this
+pub fn calculate_seq_with_wildcards(qs: &str, quals: Option<Array1<u8>>, base_wildcard_threshold: u8) -> String {
+    if let Some(qls) = quals {
+        zip(qs.chars(), qls).map(|(c, q)| {
+            if q > base_wildcard_threshold { c } else { 'X' }
+        }).collect::<String>()
+    } else {
+        qs.to_owned()
+    }
+}
+
+#[pyfunction]
+#[pyo3(name = "calculate_seq_with_wildcards")]
+pub fn calculate_seq_with_wildcards_py(
+    qs: PyBackedStr,
+    quals: Option<&Bound<'_, PyArray1<u8>>>,
+    base_wildcard_threshold: u8,
+) -> String {
+    if let Some(qls) = quals {
+        let qls_ro = qls.readonly();
+        zip(qs.chars(), qls_ro.as_array()).map(|(c, &q)| {
+            if q > base_wildcard_threshold { c } else { 'X' }
+        }).collect::<String>()
+    } else {
+        qs.to_string()
+    }
 }
