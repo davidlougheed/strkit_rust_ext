f91f9a7fdfadcda0c6643367bf31a6d77962c65c
David Lougheed <david.lougheed@gmail.com>
2025-09-16 13:53:24 -0400

    test: fix repeat tests + parametrize

--- tests/test_repeats.py
+++ tests/test_repeats.py
@@ -1,11 +1,33 @@
+import pytest
 from strkit_rust_ext import get_repeat_count
 
 
-def test_get_repeat_count():
-    assert get_repeat_count(5, "CACACACACA", "ATGC", "ATGC", "CA", 50, 3, 1, True) == ((5, 36), 1, 0)
-    assert get_repeat_count(5, "CACACACA", "ATGC", "ATGC", "CA", 50, 3, 1, True) == ((4, 32), 5, -1)
-    assert get_repeat_count(4, "CACACCCC", "ATGC", "ATGC", "CA", 50, 3, 1, True) == ((4, 14), 5, 0)
-
-    assert get_repeat_count(5, "CACACACACA", "ATGC", "ATGC", "CA", 50, 3, 1, False) == ((5, 36), 1, 0)
-    assert get_repeat_count(5, "CACACACA", "ATGC", "ATGC", "CA", 50, 3, 1, False) == ((4, 32), 5, -1)
-    assert get_repeat_count(4, "CACACCCC", "ATGC", "ATGC", "CA", 50, 3, 1, False) == ((4, 14), 5, 0)
+@pytest.mark.parametrize("grc_args,result", [
+    (
+        (5, "CACACACACA", "ATGC", "ATGC", "CA", 50, 3, 1, True),
+        ((5, 36), 1, 0),
+    ),
+    (
+        (5, "CACACACA", "ATGC", "ATGC", "CA", 50, 3, 1, True),
+        ((4, 32), 5, -1),
+    ),
+    (
+        (4, "CACACCCC", "ATGC", "ATGC", "CA", 50, 3, 1, True),
+        ((4, 14), 5, 0),
+    ),
+    # ----
+    (
+        (5, "CACACACACA", "ATGC", "ATGC", "CA", 50, 3, 1, False),
+        ((5, 36), 1, 0),
+    ),
+    (
+        (5, "CACACACA", "ATGC", "ATGC", "CA", 50, 3, 1, False),
+        ((4, 32), 9, -1),
+    ),
+    (
+        (4, "CACACCCC", "ATGC", "ATGC", "CA", 50, 3, 1, False),
+        ((4, 14), 9, 0),
+    ),
+])
+def test_get_repeat_count(grc_args, result):
+    assert get_repeat_count(*grc_args) == result
